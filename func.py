886e99e6af9db266c79ad119bdc5218d4da66fcb
Abdumalikov Sardor <sardorlearnjs@gmail.com>
2024-01-27 14:55:01 +0700

    готовый проект

--- func.py
+++ func.py
@@ -1,16 +1,21 @@
 import json
 from datetime import datetime
 
+def open_file():
+    dates = []
+    with open('operations.json', encoding='utf-8') as f:
+        data = json.load(f)
+        for oper in data:
+            if oper: #проверяем есть ли пустой словарь
+                date_string = oper['date']
 
-with open('operations.json', encoding='utf-8') as f:
-    data = json.load(f)
-    for id, oper in enumerate(data):
-        if not oper: #проверяем есть ли пустой словарь
-            continue # если есть, пропускаем
-        date_string = data[id]["date"]
-        # преобразовываем строку в формат год, часы, минуты, секунды, миллисекунды
-        date_time = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%f')
-        # print(date_time)
-        #форматируем в строку
-        formatted_date_time = date_time.strftime('%Y-%m-%d %H:%M:%S %f')[:-3]
-        print(formatted_date_time)
+                # преобразовываем строку в формат год, часы, минуты, секунды, миллисекунды
+                date_time = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%S.%f')
+
+                #форматируем в строку
+                formatted_date_time = date_time.strftime('%Y-%m-%d %H:%M:%S %f')[:-3]
+                dates.append((formatted_date_time, oper))
+
+        sorted_list = sorted(dates, key=lambda x: datetime.strptime(x[0], '%Y-%m-%d %H:%M:%S %f'), reverse=True)
+        return [item[1] for item in sorted_list]
+
---
+++ main.py
@@ -0,0 +1,16 @@
+from message import *
+
+def operations():
+    operation = get_dates()
+    result = []
+    for item in operation:
+        if not 'from' in item:
+            result.append(f"{item['date']} {item['description']} {item['to']} {item['operationAmount']['amount']} {item['operationAmount']['currency']['name']}")
+        else:
+            result.append(f"{item['date']} {item['description']} {item['from']} -> {item['to']} {item['operationAmount']['amount']} {item['operationAmount']['currency']['name']}")
+
+    return '\n'.join(result)
+
+if __name__ == "__main__":
+    # print("Script is run directly")
+    operations()
